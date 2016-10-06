4734d175993ca76ff8820fb70309f880444a5246
Lucas Maia e Silva <lucasmaiaesilva@gmail.com>
2016-10-06 13:37:29 -0300

    Removing the jquery and making the request with pure JS (#35)

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-// Choose branch based on query string
-var query = window.location.search;
-if (query) {
-	var branch = query.replace("?", "");
-} else {
-	var branch = "master";
-}
-
-// Load readme content
-$.ajax({
-	url: "https://rawgit.com/pragmaticivan/guia-do-trabalho-remoto/"+branch+"/README.md",
-	dataType: 'text',
-	success: function(data) {
-
-		// Convert readme from markdown to html
-		var converter = new Markdown.Converter();
-
-		// Show html
-		$(".container").html(converter.makeHtml(data));
-
+(function(win){
+	'use strict';
+	// Choose branch based on query string
+	var query = win.location.search;
+	var data;
+	if (query) {
+		var branch = query.replace("?", "");
+	} else {
+		var branch = "master";
 	}
-});
+
+	// Load readme content
+	function getData() {
+		var url = 'https://rawgit.com/pragmaticivan/guia-do-trabalho-remoto/'+branch+'/README.md';
+		var ajax = new XMLHttpRequest();
+		ajax.open('GET', url);
+		ajax.send();
+		ajax.addEventListener('readystatechange', handleRequest, false);
+	}
+
+	function handleRequest() {
+		if(isRequestOk(this)) {
+			data = this.responseText;
+			var converter = new Markdown.Converter();
+			var $container = document.querySelector('.container');
+			$container.innerHTML = converter.makeHtml(data);
+		}
+	}
+
+	function isRequestOk(ajaxGet) {
+		return ajaxGet.readyState === 4;
+	}
+
+	getData();
+}(window));
