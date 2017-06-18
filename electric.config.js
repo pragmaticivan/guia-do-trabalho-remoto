aafaa8a02ffd0ff925aa4653b09215338a8904fb
Ivan Santos <pragmaticivan@gmail.com>
2017-06-17 22:09:01 -0700

    Adds editorconfig

--- electric.config.js
+++ electric.config.js
@@ -3,9 +3,10 @@
 var marble = require('marble');
 
 module.exports = {
-	metalComponents: ['electric-marble-components'],
-	sassOptions: {
-		includePaths: ['node_modules', marble.src]
-	},
-	vendorSrc: ['node_modules/marble/build/fonts/**']
+  metalComponents: ['electric-marble-components'],
+  sassOptions: {
+    includePaths: ['node_modules', marble.src]
+  },
+  vendorSrc: ['node_modules/marble/build/fonts/**'],
+  deployOptions: { branch: 'gh-pages' }
 };
